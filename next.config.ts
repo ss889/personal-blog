d9736c6369ffe9b9974a1fda2ca97ceea1ece03d
ss889 <ss889@njit.edu>
2026-03-04 13:33:00 -0500

    Add basePath for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: isProd ? "/personal-blog" : "",
+  assetPrefix: isProd ? "/personal-blog/" : "",
   reactCompiler: true,
   images: {
     unoptimized: true,
